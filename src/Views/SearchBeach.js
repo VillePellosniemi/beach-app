7ab3a50ae6f4f28f870ed17c77b10463e9e92fee
Lauri Salminen <lauriaus@metropolia.fi>
2020-10-27 16:14:13 +0200

    searchUsesHomeJS

--- src/Views/SearchBeach.js
+++ src/Views/SearchBeach.js
@@ -2,8 +2,40 @@ import React from 'react';
 import Loader from '../Components/Loader';
 import { Link } from 'react-router-dom';
 import { useAxiosGet } from '../Hooks/HttpRequests';
+import Home from '../Views/Home'
 
 class SearchBeach extends React.Component {
+  /* Home() {
+    const url = 'https://iot.fvh.fi/opendata/uiras/uiras2_v1.json'
+    let beaches = useAxiosGet(url)
+    let content = null
+
+    if(beaches.loading) {
+      content = <Loader />
+    }
+
+    if(beaches.dt) {
+      content = Object.keys(beaches.dt).map((beach, key) => {
+        //get beach ID from API
+        const id = Object.values(beach).join("")
+
+        return (
+
+            <h1 className = "p-5" key = {key}>
+              <Link to = {`/${id}`}>
+                {beaches.dt[beach].meta.name}
+              </Link>
+            </h1>
+        )
+      })
+    }
+
+    return (
+        <div> {content} </div>
+    )
+  }
+*/
+
      constructor(props){
          console.log(props)
         super(props);
@@ -12,12 +44,13 @@ class SearchBeach extends React.Component {
         };
     }
 
+
     render(){
         const {beachName} = this.state;
+
         return(
-            <React.Fragment>
-                {beachName}
-            </React.Fragment>
+
+            <Home/>
 
         )
     }
